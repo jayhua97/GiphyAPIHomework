e5c5f3ea6067e8f9e52c74b5a3baaf137eb0d7e9
Jayhua <jordanhua97@gmail.com>
2018-08-13 21:51:09 -0700

    empty input html after search. add a bit of css. added ratings to each gif

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,41 +1,45 @@
-var topics = [];
-
+var topics = ['cats', 'dogs', 'fish', 'mouse'];
+var gifCounter = 0;
 
 $(document).on("click",".topic", function() {
     event.preventDefault();
     var topic = ($(this).attr("data-name")) ;
     var apiKey = "vh7aVQK5fbGTKZONe6lqziwXU70heo63";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apiKey + "&limit=1";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=${'random'}" + topic + "&api_key=" + apiKey + "&limit=10";
     $.ajax({
 
         url: queryURL,
         method: "GET"
       
       }).then(function(response) {
+        console.log(response);
+        for (var i = 0; i < topics.length; i++) {
         $("#gifs-go-here").append("<div id='gifs'>");
+        // $("#gifs-go-here").append("<div id='gifCount-" + gifCounter + "'>")
+        // $("#gifCount").append("<div id='gifs'>");
         var results = response.data;
         var topicImage = $("<img>");
-        topicImage.attr("src", results[0].images.fixed_height_still.url)
-        topicImage.attr("data-animate", results[0].images.fixed_height.url)
-        topicImage.attr("data-still", results[0].images.fixed_height_still.url)
+        topicImage.attr("src", results[i].images.fixed_height_still.url)
+        topicImage.attr("data-animate", results[i].images.fixed_height.url)
+        topicImage.attr("data-still", results[i].images.fixed_height_still.url)
         topicImage.attr("data-state", "still");
         topicImage.addClass("gif");
         $("#gifs").append(topicImage);
+        $("#gifs").append("<p> Rating: " + results[0].rating + "</p>");
         $(".gif").on("click", function() {
-            console.log("hello")
             var state = $(this).attr("data-state");
             if (state === "still") {
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr("data-state", "animate");
-                console.log("hellooo");
             }
             else {
                 $(this).attr("src",$(this).attr("data-still"));
                 $(this).attr("data-state", "still");
             }
         })
+        }
       });
-    
+    gifCounter++;
 })
 
 
@@ -47,7 +51,7 @@ function renderButton() {
         topicButton.addClass("btn");
         topicButton.addClass("btn-dark")
         topicButton.attr("data-name", topics[i]);
-        topicButton.text(topics[i]);
+        topicButton.text(topics[i])
         $("#buttonList").append(topicButton);
         console.log("hello")
     }
@@ -58,6 +62,12 @@ $("#add-gif").on("click", function(event) {
     event.preventDefault();
     var gif = $("#gif-input").val().trim();
     topics.push(gif);
-    console.log("yo")
     renderButton();
 })
+
+$("input[type='submit']").on("click", function(event) {
+    event.preventDefault();
+    // Setting the input value to a variable and then clearing the input
+    $("input[type='text']").val("");
+})
+renderButton();
